923096c90a845565d975e6e8757a1ee39bc62747
PozzITiv4ik <ivankravzovopenai@gmail.com>
2025-12-30 18:38:50 +0100

    Move character translations into module

---
+++ js/characters/characters-texts.js
@@ -0,0 +1,49 @@
+window.SCRAPPO_I18N = window.SCRAPPO_I18N || {};
+
+window.SCRAPPO_I18N.en = {
+  ...(window.SCRAPPO_I18N.en || {}),
+  "characters.title": "Select Character",
+  "characters.subtitle": "Pick a fighter and jump into the run.",
+  "characters.section.stats": "Stats",
+  "characters.section.traits": "Traits",
+  "characters.traits.none": "No bonuses",
+  "characters.tag.base": "Base",
+  "characters.normal.name": "Standard",
+  "characters.normal.desc": "Balanced starter with no penalties.",
+  "characters.normal.note1": "Stable stats across the board.",
+  "characters.psycho.name": "Psycho",
+  "characters.psycho.desc": "High damage, lower survivability.",
+  "characters.psycho.note1": "Hit harder from the start.",
+  "characters.psycho.note2": "Less max health.",
+  "characters.agile.name": "Agile",
+  "characters.agile.desc": "Fast and light on the field.",
+  "characters.agile.note1": "Higher movement speed.",
+  "characters.agile.note2": "Wider pickup radius.",
+  "characters.normal": "1. Standard",
+  "characters.psycho": "2. Psycho",
+  "characters.agile": "3. Agile"
+};
+
+window.SCRAPPO_I18N.ru = {
+  ...(window.SCRAPPO_I18N.ru || {}),
+  "characters.title": "Выбор персонажа",
+  "characters.subtitle": "Выбери персонажа и начни забег.",
+  "characters.section.stats": "Характеристики",
+  "characters.section.traits": "Особенности",
+  "characters.traits.none": "Без бонусов",
+  "characters.tag.base": "База",
+  "characters.normal.name": "Обычный",
+  "characters.normal.desc": "Сбалансированный старт без штрафов.",
+  "characters.normal.note1": "Ровные характеристики.",
+  "characters.psycho.name": "Псих",
+  "characters.psycho.desc": "Больше урона, меньше живучесть.",
+  "characters.psycho.note1": "Бьет больнее с начала.",
+  "characters.psycho.note2": "Меньше максимум здоровья.",
+  "characters.agile.name": "Ловкий",
+  "characters.agile.desc": "Быстрый и легкий в управлении.",
+  "characters.agile.note1": "Повышенная скорость.",
+  "characters.agile.note2": "Больший радиус подбора.",
+  "characters.normal": "1. Обычный",
+  "characters.psycho": "2. Псих",
+  "characters.agile": "3. Ловкий"
+};
